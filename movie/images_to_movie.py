3c64705e22e514b7d31bd950705bfbc8fe1a3285
otenim <snake_istobelieve@keio.jp>
2019-01-09 02:32:01 +0900

    first commit

---
+++ movie/images_to_movie.py
@@ -0,0 +1,11 @@
+import cv2
+import argparse
+import os
+
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+parser = argparse.ArgumentParser()
+parser
+parser.add_argument('cfg_path')
+parser.add_argument('data_path')
+parser.add_argument('weights_path')
